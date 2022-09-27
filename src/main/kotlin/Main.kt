f0f65546c5d4fdc0a1872d3576a556ed787872e2
JDeegan20 <20085768@mail.wit.ie>
2022-09-27 16:01:02 +0100

    Employee App - Reworking Layout

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,36 +13,38 @@ val annualBonus = 1450.50
 val cycleToWorkMonthlyDeduction = 54.33
 
 fun main(args: Array<String>){
-    println("Payslip Printer")
     printPayslip()
 }
 
-fun printPayslip(){
+fun printPayslip() {
 
-    val monthlySalary   = roundTwoDecimals(grossSalary/12)
-    val monthlyPrsi     = roundTwoDecimals(monthlySalary * (prsiPercentage / 100))
-    val monthlyPaye     = roundTwoDecimals(monthlySalary * (payePercentage / 100))
-    val grossPay        = roundTwoDecimals(monthlySalary + (annualBonus/12))
+    val monthlySalary = roundTwoDecimals(grossSalary / 12)
+    val monthlyPrsi = roundTwoDecimals(monthlySalary * (prsiPercentage / 100))
+    val monthlyPaye = roundTwoDecimals(monthlySalary * (payePercentage / 100))
+    val grossPay = roundTwoDecimals(monthlySalary + (annualBonus / 12))
     val totalDeductions = roundTwoDecimals((monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction))
 
-    println ("_________________________________________________________________________")
-    println ("|                          Monthly Payslip                              |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|   Employee Name:  ${firstName.uppercase()} ${surname.uppercase()} (${gender.uppercase()})                   Employee ID: $employeeID    |")
-    println ("|                                                                       |")
-    println ("|_______________________________________________________________________|")
-    println ("|                                                                       |")
-    println ("|    PAYMENT DETAILS \t\t\t\t DEDUCTION DETAILS  \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Salary: $monthlySalary \t\t\t\t PAYE: $monthlyPaye \t\t\t\t\t\t|")
-    println ("|    Bonus:  ${roundTwoDecimals(annualBonus/12)} \t\t\t\t PRSI: $monthlyPrsi  \t\t\t\t\t\t|")
-    println ("|    \t\t\t\t\t\t\t\t Cycle To Work: $cycleToWorkMonthlyDeduction \t\t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|    Gross: $grossPay \t\t\t\tTotal Deductions: $totalDeductions \t\t\t|")
-    println ("|_______________________________________________________________________|")
-    println ("|   \t\t\t\t\t\t NET PAY: ${roundTwoDecimals(grossPay - totalDeductions)}  \t\t\t\t\t\t\t|")
-    println ("|_______________________________________________________________________|")
+    println(
+        """
+        ______________________________________________________________________
+         Monthly Payslip:             ${firstName.uppercase()} ${surname.uppercase()} (${gender.uppercase()}), ID: $employeeID                  
+        ______________________________________________________________________    
+              PAYMENT DETAILS (gross pay: $grossPay)                                                                    
+        ______________________________________________________________________
+                   Salary: $monthlySalary
+                   Bonus:  ${roundTwoDecimals(annualBonus / 12)}            
+        ______________________________________________________________________
+              DEDUCTION DETAILS (total Deductions: $totalDeductions)      
+        ______________________________________________________________________
+                   PAYE: $monthlyPaye                
+                   PRSI: $monthlyPrsi  
+                   Cycle To Work: $cycleToWorkMonthlyDeduction         
+        ______________________________________________________________________
+             NET PAY: ${roundTwoDecimals(grossPay - totalDeductions)} 
+        ______________________________________________________________________"""
+    )
 }
 
 fun roundTwoDecimals(number: Double) = round(number * 100) / 100
+
+
